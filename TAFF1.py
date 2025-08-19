7b40b8f7d84a39f23c4d2d7f5991be5e4b0e7fdc
Mairasow <sowmairamecoll@gmail.com>
2025-08-19 13:51:04 +0000

    modification du TAFF1.py en retournant la somme

--- TAFF1.py
+++ TAFF1.py
@@ -1,11 +1,8 @@
-TAFF1:
-écrire un programme qui permet de saisir deux nombres et d'afficher leur produit.
 m=float(input("entrer le premier nombre:"))
-entrer le premier nombre:7
 n=float(input("entrer le seconde nombre: "))
-entrer le seconde nombre:8
-produit= 7*8
-print("le produit de",m,"et",n,"=",p)
-le produit de 7.0et 8.0 = 56.0
+somme= m+n
+print("la somme de m et n est:",somme)
+print("modification du taf1 en retournant la somme")
+
 
     
